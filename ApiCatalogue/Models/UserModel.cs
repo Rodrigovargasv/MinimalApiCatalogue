f947474dced2c3ddb106c508be83c61c63402038
Rodrigovargasv <rodrigovargasv80@gmail.com>
2023-02-09 16:44:23 -0300

    Instituted and configured the JWT library in the project and created the UserModel class and its attributes, finally created the Services folder and the ITokenService interface and its attributes

---
+++ ApiCatalogue/Models/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiCatalogue.Models
+{
+    public class UserModel
+    {
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ ApiCatalogue/Services/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using ApiCatalogue.Models;
+
+namespace ApiCatalogue.Services;
+public interface ITokenService
+{
+    string GenerationToken(string key, string issuer, string audience, UserModel userModel);
+
+}
+
